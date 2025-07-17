194ae3867b1a5c4f8b4dd3a877605aba169cc95d
Maestro Ezra <mentegaest@Maestros-MacBook-Air.local>
2025-07-17 18:41:39 +0800

    hapus text pratinjau

--- user/gantipassword.php
+++ user/gantipassword.php
@@ -25,14 +25,14 @@
     </div>
 </div>
 
-<!-- Button Kembali -->
+<!-- Button Kembali
 <div class="container-fluid" style="margin-bottom: 20px;">
     <div class="row">
         <div class="col-lg-12 style">
             <a href="arsip.php" class="btn btn-sm" style="background-color: #404040; color: white;"><i class="fa fa-arrow-left" style="color: white !important;"></i> Kembali</a>
         </div>
     </div>
-</div>
+</div> -->
 
 
 <div class="container-fluid">
@@ -72,10 +72,13 @@
     </div>
 </div>
 
-<br>
-<br>
-<br>
-<br>
+<!-- div kosong -->
+<div style="padding: 100px;">
+    <br>
+    <br>
+    <br>
+</div>
+
 <br>
 <br>
 <br>
